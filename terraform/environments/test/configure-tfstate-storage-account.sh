79b983725329f70e547e4f2356449dbe98a1adde
Camilafabiani87 <camilafabiani.f@gmail.com>
2024-01-15 19:27:24 +0100

    actualizacion

--- terraform/environments/test/configure-tfstate-storage-account.sh
+++ terraform/environments/test/configure-tfstate-storage-account.sh
@@ -8,15 +8,15 @@ CONTAINER_NAME="tfstate"
 # az group create --name $RESOURCE_GROUP_NAME --location eastus
 
 # Create storage account
-az storage account create --resource-group "Azuredevops" --name "tfstate8769878" --sku Standard_LRS --encryption-services blob
+az storage account create --resource-group "Azuredevops" --name "tfstate3255918059" --sku Standard_LRS --encryption-services blob
 
 # Get storage account key
-ACCOUNT_KEY=$(az storage account keys list --resource-group "Azuredevops" --account-name "tfstate8769878" --query '[0].value' -o tsv)
+ACCOUNT_KEY=$(az storage account keys list --resource-group "Azuredevops" --account-name "tfstate3255918059" --query '[0].value' -o tsv)
 export ARM_ACCESS_KEY=$ACCOUNT_KEY
 
 # Create blob container
-az storage container create --name "tfstate" --account-name "tfstate8769878" --account-key "iih8Q~PoUihs.AFmPIXeGO-Sb8cHe4_VhVH9kacP"
+az storage container create --name "tfstate" --account-name "tfstate3255918059" --account-key "7yVaHkaqxwNWAVlVz5mkBOuQoHEG4bgITXDG7iejv1K2yZXeugfv6rTqo3cQedwikMFyrqssvB4v+AStWsuXYg=="
 echo "RESOURCE_GROUP_NAME=$RESOURCE_GROUP_NAME"
 echo "STORAGE_ACCOUNT_NAME=$STORAGE_ACCOUNT_NAME"
 echo "CONTAINER_NAME=$CONTAINER_NAME"
-echo "ACCOUNT_KEY= iih8Q~PoUihs.AFmPIXeGO-Sb8cHe4_VhVH9kacP"
+echo "ACCOUNT_KEY= 7yVaHkaqxwNWAVlVz5mkBOuQoHEG4bgITXDG7iejv1K2yZXeugfv6rTqo3cQedwikMFyrqssvB4v+AStWsuXYg=="
